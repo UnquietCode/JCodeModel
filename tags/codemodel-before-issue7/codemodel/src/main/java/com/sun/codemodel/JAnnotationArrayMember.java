7ab11fea588233d2fac7ba4f09e106e3d843f83f
lexi <lexi@1b0cd94c-d507-0410-907c-c8211361794a>
2008-04-24 07:51:28 +0000

    git-svn-id: https://svn.java.net/svn/codemodel~svn@124 1b0cd94c-d507-0410-907c-c8211361794a

---
+++ tags/codemodel-before-issue7/codemodel/src/main/java/com/sun/codemodel/JAnnotationArrayMember.java
@@ -0,0 +1,169 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.codemodel;
+
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents an arrays as annotation members
+ *
+ * <p>
+ * This class implements {@link JAnnotatable} to allow
+ * new annotations to be added as a member of the array.
+ *
+ * @author
+ *     Bhakti Mehta (bhakti.mehta@sun.com)
+ */
+public final class JAnnotationArrayMember extends JAnnotationValue implements JAnnotatable {
+    private final List<JAnnotationValue> values = new ArrayList<JAnnotationValue>();
+    private final JCodeModel owner;
+
+    JAnnotationArrayMember(JCodeModel owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Adds an array member to this annotation
+     *
+     * @param value Adds a string value to the array member
+     * @return The JAnnotationArrayMember. More elements can be added by calling
+     *         the same method multiple times
+     */
+    public JAnnotationArrayMember param(String value) {
+        JAnnotationValue annotationValue = new JAnnotationStringValue(JExpr.lit(value));
+        values.add(annotationValue);
+        return this;
+    }
+
+    public JAnnotationArrayMember param(boolean value) {
+        JAnnotationValue annotationValue = new JAnnotationStringValue(JExpr.lit(value));
+        values.add(annotationValue);
+        return this;
+    }
+
+    /**
+     * Adds an array member to this annotation
+     *
+     * @param value Adds an int value to the array member
+     * @return The JAnnotationArrayMember. More elements can be added by calling
+     *         the same method multiple times
+     */
+    public JAnnotationArrayMember param(int value) {
+        JAnnotationValue annotationValue = new JAnnotationStringValue(JExpr.lit(value));
+        values.add(annotationValue);
+        return this;
+    }
+
+    /**
+     * Adds an array member to this annotation
+     *
+     * @param value Adds a float value to the array member
+     * @return The JAnnotationArrayMember. More elements can be added by calling
+     *         the same method multiple times
+     */
+    public JAnnotationArrayMember param(float value) {
+        JAnnotationValue annotationValue = new JAnnotationStringValue(JExpr.lit(value));
+        values.add(annotationValue);
+        return this;
+    }
+
+    public JAnnotationArrayMember param(Class value){
+       JAnnotationValue annotationValue = new JAnnotationStringValue(JExpr.lit(value.getName()));
+       values.add(annotationValue);
+       return this;
+   }
+
+    public JAnnotationArrayMember param(JType type){
+        JClass clazz = type.boxify();
+        JAnnotationValue annotationValue = new JAnnotationStringValue ( clazz.dotclass() );
+        values.add(annotationValue);
+        return this;
+    }
+
+    /**
+     * Adds a new annotation to the array.
+     */
+    public JAnnotationUse annotate(Class<? extends Annotation> clazz){
+        return annotate(owner.ref(clazz));
+    }
+
+    /**
+     * Adds a new annotation to the array.
+     */
+    public JAnnotationUse annotate(JClass clazz){
+        JAnnotationUse a = new JAnnotationUse(clazz);
+        values.add(a);
+        return a;
+    }
+
+    public <W extends JAnnotationWriter> W annotate2(Class<W> clazz) {
+        return TypedAnnotationWriter.create(clazz,this);
+    }
+
+    /**
+     * Adds an annotation member to this annotation  array
+     * This can be used for e.g &#64;XmlCollection(values= &#64;XmlCollectionItem(type=Foo.class))
+     * @param value
+     *        Adds a annotation  to the array member
+     * @return
+     *        The JAnnotationArrayMember. More elements can be added by calling
+     *        the same method multiple times
+     *
+     * @deprecated
+     *      use {@link #annotate}
+     */
+    public JAnnotationArrayMember param (JAnnotationUse value ){
+        values.add(value);
+        return this;
+    }
+
+    public void generate(JFormatter f) {
+        f.p('{').nl().i();
+
+        boolean first = true;
+        for (JAnnotationValue aValue : values) {
+            if (!first)
+                f.p(',').nl();
+            f.g(aValue);
+            first = false;
+        }
+        f.nl().o().p('}');
+    }
+}
+
