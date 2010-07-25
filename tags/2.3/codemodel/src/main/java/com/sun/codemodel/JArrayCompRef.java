28f760a24ba76fddfc1151f7e0904375005ab773
lexi <lexi@1b0cd94c-d507-0410-907c-c8211361794a>
2010-07-25 12:11:10 +0000

    [maven-release-plugin]  copy for tag 2.3
    git-svn-id: https://svn.java.net/svn/codemodel~svn@193 1b0cd94c-d507-0410-907c-c8211361794a

---
+++ tags/2.3/codemodel/src/main/java/com/sun/codemodel/JArrayCompRef.java
@@ -0,0 +1,82 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.codemodel;
+
+/**
+ * array component reference.
+ */
+final class JArrayCompRef extends JExpressionImpl implements JAssignmentTarget {
+    /**
+     * JArray expression upon which this component will be accessed.
+     */
+    private final JExpression array;
+
+    /**
+     * Integer expression representing index of the component
+     */
+    private final JExpression index;
+
+    /**
+     * JArray component reference constructor given an array expression
+     * and index.
+     *
+     * @param array
+     *        JExpression for the array upon which
+     *        the component will be accessed,
+     *
+     * @param index
+     *        JExpression for index of component to access
+     */
+    JArrayCompRef(JExpression array, JExpression index) {
+        if ((array == null) || (index == null)) {
+            throw new NullPointerException();
+        }
+        this.array = array;
+        this.index = index;
+    }
+
+    public void generate(JFormatter f) {
+        f.g(array).p('[').g(index).p(']');
+    }
+
+    public JExpression assign(JExpression rhs) {
+		return JExpr.assign(this,rhs);
+    }
+    public JExpression assignPlus(JExpression rhs) {
+		return JExpr.assignPlus(this,rhs);
+    }
+}
