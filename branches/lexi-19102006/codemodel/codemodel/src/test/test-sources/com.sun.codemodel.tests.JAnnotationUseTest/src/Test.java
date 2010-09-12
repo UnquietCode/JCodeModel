934736ed0c42eeb2e3271f5d9bce49cec61c39f7
lexi <lexi@1b0cd94c-d507-0410-907c-c8211361794a>
2010-09-12 09:28:46 +0000

    Onsolete
    git-svn-id: https://svn.java.net/svn/codemodel~svn@226 1b0cd94c-d507-0410-907c-c8211361794a

--- branches/lexi-19102006/codemodel/codemodel/src/test/test-sources/com.sun.codemodel.tests.JAnnotationUseTest/src/Test.java
+++
@@ -1,9 +0,0 @@
-
-@com.sun.codemodel.tests.JAnnotationUseTest.A(classField = java.lang.String.class, classArrayField = {
-    java.lang.String.class,
-    java.lang.Long.class
-})
-public class Test {
-
-
-}
